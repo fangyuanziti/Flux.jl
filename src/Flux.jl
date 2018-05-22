5a7f7c0769b5c37268191252c56f2cd49802ae01
Mike J Innes <mike.j.innes@gmail.com>
2018-05-22 12:11:28 +0800

    wip julia 0.7

--- src/Flux.jl
+++ src/Flux.jl
@@ -4,7 +4,7 @@ module Flux
 
 # Zero Flux Given
 
-using Juno, Requires, Reexport
+using Requires, Reexport
 using MacroTools: @forward
 
 export Chain, Dense, RNN, LSTM, GRU, Conv,
@@ -25,6 +25,7 @@ using .Optimise: @epochs
 export SGD, ADAM, AdaMax, Momentum, Nesterov,
        RMSProp, ADAGrad, ADADelta, AMSGrad
 
+include("oset.jl")
 include("utils.jl")
 include("onehot.jl")
 include("treelike.jl")
@@ -35,7 +36,7 @@ include("layers/conv.jl")
 include("layers/recurrent.jl")
 include("layers/normalise.jl")
 
-include("data/Data.jl")
+# include("data/Data.jl")
 
 @require CuArrays include("cuda/cuda.jl")
 
